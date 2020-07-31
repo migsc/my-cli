5171bbd6f1e972ddc9bbfe755606cbaf5aa5dad0
Miguel Chateloin <mchateloin@cenergistic.com>
2020-07-31 16:22:23 -0500

    fix snippets command

---
+++ src/commands/snips/refresh/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Text, Box } from "ink";
+
+const propTypes = {};
+
+/// Snippets refresh command
+const refresh = ({ name }) => {
+	return <Text>Coming soon.</Text>;
+};
+
+refresh.propTypes = propTypes;
+
+export default refresh;
